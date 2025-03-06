dd23398ff6c21588983d4cd614151a6a3f536576
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2025-03-06 22:29:28 +0800

    refac: added validation on server action.

--- src/app/(main)/assets/_actions/register-assets.ts
+++ src/app/(main)/assets/_actions/register-assets.ts
@@ -5,11 +5,13 @@ import { revalidatePath } from "next/cache"
 import Assets from "@/models/assets"
 import connect from "@/lib/database"
 
-import assetSchema from "../form-schema"
+import schema from "../form-schema"
 
 export interface FormState {
+  success: boolean
   message: string
   fields?: Record<string, string>
+  issues?: string[]
 }
 
 export async function onRegisterAsset(
@@ -19,14 +21,31 @@ export async function onRegisterAsset(
   const data = Object.fromEntries(formData)
 
   try {
+    const parsed = schema.safeParse(data)
+
+    if (!parsed.success) {
+      const fields: Record<string, string> = {}
+
+      for (const key of Object.keys(formData)) {
+        fields[key] = data[key].toString()
+      }
+
+      return {
+        success: false,
+        fields,
+        message: "Error in asset registration.",
+        issues: parsed.error.issues.map(issue => issue.message)
+      }
+    }
+
     await connect()
     await Assets.create({ ...data })
 
     revalidatePath("/assets")
 
-    return { message: "success" }
+    return { success: true, message: "Device successfully registered" }
   } catch (error) {
     console.log("registerAsset action error: ", error)
-    return { message: "error" }
+    return { success: false, message: "error" }
   }
 }

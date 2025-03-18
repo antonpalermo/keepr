3fc8a8e2086117cfbabb1b2dd27ff16d9a7723dd
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2025-03-18 23:06:05 +0800

    refac: update date format

--- src/app/(main)/assets/_components/columns.tsx
+++ src/app/(main)/assets/_components/columns.tsx
@@ -19,14 +19,32 @@ const columns: ColumnDef<Asset>[] = [
     header: "Name",
     cell: ({ row }) => {
       return (
-        <Button variant={"link"} asChild>
+        <Button variant={"link"} asChild className="mx-0 px-0">
           <Link href={`/assets/${row.original.id}`}>{row.original.name}</Link>
         </Button>
       )
     }
   },
-  { accessorKey: "dateCreated", header: "Date Created" },
-  { accessorKey: "dateUpdated", header: "Date Updated" },
+  {
+    accessorKey: "dateCreated",
+    header: "Date Created",
+    cell: ({ row }) => {
+      const createdDate = new Date(row.original.dateCreated)
+      return Intl.DateTimeFormat("en-US", { dateStyle: "long" }).format(
+        createdDate
+      )
+    }
+  },
+  {
+    accessorKey: "dateUpdated",
+    header: "Date Updated",
+    cell: ({ row }) => {
+      const dateUpdated = new Date(row.original.dateUpdated)
+      return Intl.DateTimeFormat("en-US", { dateStyle: "long" }).format(
+        dateUpdated
+      )
+    }
+  },
   {
     id: "actions",
     enableHiding: false,

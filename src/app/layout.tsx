9f89d2432a225d753d852004e60f72491bd6e778
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2025-03-30 22:54:56 +0800

    init next-auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,12 @@
 import { Inter } from "next/font/google"
+import { getServerSession } from "next-auth"
 
 import { cn } from "@/lib/utils"
+
+import SessionProvider from "@/components/providers/session"
 import QueryClientProvider from "@/components/providers/query-client"
+
+import { options } from "@/app/api/auth/options"
 
 import "./global.css"
 
@@ -15,15 +20,19 @@ const inter = Inter({
   display: "swap"
 })
 
-export default function RootLayout({
+export default async function RootLayout({
   children
 }: {
   children: React.ReactNode
 }) {
+  const session = await getServerSession(options)
+
   return (
     <html lang="en">
       <body className={cn(inter.className, "min-h-screen")}>
-        <QueryClientProvider>{children}</QueryClientProvider>
+        <SessionProvider session={session}>
+          <QueryClientProvider>{children}</QueryClientProvider>
+        </SessionProvider>
       </body>
     </html>
   )

c13990822816e75c6237b0ca1fef25cfb3f5b70e
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2025-03-31 23:15:46 +0800

    added next-atuth schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -4,22 +4,118 @@ import {
   pgTable,
   timestamp,
   uniqueIndex,
-  uuid,
-  varchar
+  varchar,
+  text,
+  integer,
+  primaryKey,
+  boolean
 } from "drizzle-orm/pg-core"
+import type { AdapterAccount } from "next-auth/adapters"
+
+export const users = pgTable(
+  "users",
+  {
+    id: text("id")
+      .primaryKey()
+      .$defaultFn(() => crypto.randomUUID()),
+    name: text("name"),
+    email: text("email").unique(),
+    emailVerified: timestamp("emailVerified", { mode: "date" }),
+    image: text("image")
+  },
+  table => [
+    index("id_user_index").on(table.id),
+    uniqueIndex("id_user_unique_index").on(table.id),
+    index("email_index").on(table.email),
+    uniqueIndex("email_unique_index").on(table.email)
+  ]
+)
+
+export const accounts = pgTable(
+  "accounts",
+  {
+    userId: text("userId")
+      .notNull()
+      .references(() => users.id, { onDelete: "cascade" }),
+    type: text("type").$type<AdapterAccount>().notNull(),
+    provider: text("provider").notNull(),
+    providerAccountId: text("providerAccountId").notNull(),
+    refresh_token: text("refresh_token"),
+    access_token: text("access_token"),
+    expires_at: integer("expires_at"),
+    token_type: text("token_type"),
+    scope: text("scope"),
+    id_token: text("id_token"),
+    session_state: text("session_state")
+  },
+  account => [
+    {
+      compoundKey: primaryKey({
+        columns: [account.provider, account.providerAccountId]
+      })
+    }
+  ]
+)
+
+export const sessions = pgTable("session", {
+  sessionToken: text("sessionToken").primaryKey(),
+  userId: text("userId")
+    .notNull()
+    .references(() => users.id, { onDelete: "cascade" }),
+  expires: timestamp("expires", { mode: "date" }).notNull()
+})
+
+export const verificationTokens = pgTable(
+  "verification_tokens",
+  {
+    identifier: text("identifier").notNull(),
+    token: text("token").notNull(),
+    expires: timestamp("expires", { mode: "date" }).notNull()
+  },
+  verificationToken => [
+    {
+      compositePk: primaryKey({
+        columns: [verificationToken.identifier, verificationToken.token]
+      })
+    }
+  ]
+)
+
+export const authenticators = pgTable(
+  "authenticators",
+  {
+    credentialID: text("credentialID").notNull().unique(),
+    userId: text("userId")
+      .notNull()
+      .references(() => users.id, { onDelete: "cascade" }),
+    providerAccountId: text("providerAccountId").notNull(),
+    credentialPublicKey: text("credentialPublicKey").notNull(),
+    counter: integer("counter").notNull(),
+    credentialDeviceType: text("credentialDeviceType").notNull(),
+    credentialBackedUp: boolean("credentialBackedUp").notNull(),
+    transports: text("transports")
+  },
+  authenticator => [
+    {
+      compositePK: primaryKey({
+        columns: [authenticator.userId, authenticator.credentialID]
+      })
+    }
+  ]
+)
 
 export const asset = pgTable(
   "assets",
   {
-    id: uuid()
-      .default(sql`gen_random_uuid()`)
-      .unique(),
+    id: text("id")
+      .primaryKey()
+      .$defaultFn(() => crypto.randomUUID()),
     name: varchar({ length: 256 }),
     dateCreated: timestamp().defaultNow(),
     dateUpdated: timestamp().default(sql`now()`)
   },
   table => [
-    index("id_index").on(table.id),
-    uniqueIndex("id_unique_index").on(table.id)
+    index("id_asset_index").on(table.id),
+    uniqueIndex("id_asset_unique_index").on(table.id)
   ]
 )

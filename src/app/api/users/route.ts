91f385f87026579dcb6927b569b9e9c2d572e96e
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2025-04-09 23:02:41 +0800

    fix: not-found error on api users

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -7,6 +7,8 @@ export async function GET(request: NextRequest) {
   const searchParams = request.nextUrl.searchParams
   const email = searchParams.get("email")
 
+  console.log("fired")
+
   try {
     const result = await db
       .select()

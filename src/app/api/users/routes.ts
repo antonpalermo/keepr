205217bbdc7274649e6f01fde543c4b078bb4df6
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2025-04-09 22:48:06 +0800

    api get user by email endpoint

---
+++ src/app/api/users/routes.ts
@@ -0,0 +1,29 @@
+import { db } from "@/db"
+import { users } from "@/db/schema"
+import { sql } from "drizzle-orm"
+import { NextRequest } from "next/server"
+
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams
+  const email = searchParams.get("email")
+
+  try {
+    const result = await db
+      .select()
+      .from(users)
+      .where(
+        sql`to_tsvector('english', ${users.email}) @@ to_tsquery('english', ${email})`
+      )
+
+    return Response.json({
+      success: true,
+      data: result,
+      message: "susccessfully retrieved assets"
+    })
+  } catch (error) {
+    console.log(error)
+    return Response.json(`unable to retrieve all users with email ${email}`, {
+      status: 500
+    })
+  }
+}

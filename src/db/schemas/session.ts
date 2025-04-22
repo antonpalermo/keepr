fbcc1a1ea6dc6c2f345f85fd7d85c8cd9cd4dfa6
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2025-04-22 21:34:59 +0800

    update session schema

---
+++ src/db/schemas/session.ts
@@ -0,0 +1,19 @@
+import * as orm from "drizzle-orm/pg-core"
+import { createId } from "@paralleldrive/cuid2"
+import { user } from "./user"
+import { timestamp } from "./timestamp"
+
+export const session = orm.pgTable("sessions", {
+  id: orm
+    .text()
+    .notNull()
+    .primaryKey()
+    .$defaultFn(() => createId()),
+  sessionToken: orm.text("session_token").primaryKey(),
+  userId: orm
+    .text("user_id")
+    .notNull()
+    .references(() => user.id, { onDelete: "cascade" }),
+  expires: orm.timestamp("expires", { mode: "date" }).notNull(),
+  ...timestamp
+})

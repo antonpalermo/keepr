0a7396cced1e03f0ec7a7ab6eea56d88c38c452a
Anton Angelo Palermo <ANPBusinessmail@gmail.com>
2025-04-28 22:50:38 +0800

    update redirects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,26 @@
-import { db } from "@/db"
-import { organizations } from "@/db/schemas/organization"
 import { eq } from "drizzle-orm"
-import { getServerSession } from "next-auth"
-import { cookies } from "next/headers"
 import { redirect } from "next/navigation"
+import { getServerSession } from "next-auth"
+
+import { db } from "@/db"
+import { tenant } from "@/db/schemas/tenant"
 
 export default async function HomePage() {
   const session = await getServerSession()
 
-  const cookieStore = await cookies()
-  const currentOrganization = cookieStore.get("organization")?.value
+  // only query database if no org found in cookies
+  const tenants = await db
+    .select()
+    .from(tenant)
+    .where(eq(tenant.owner, session?.user.email || ""))
 
-  if (!currentOrganization) {
-    console.log("database call")
-
-    // only query database if no org found in cookies
-    const orgs = await db
-      .select()
-      .from(organizations)
-      .where(eq(organizations.owner, session?.user.email || ""))
-
-    // if no orgs for this user throw a not found
-    // but much better if we redirect them to create
-    // a new organization.
-    if (!orgs[0]) {
-      return redirect("/organizations/new")
-    }
-
-    // then redirect to the first organization available.
-    return redirect(`/${orgs[0].id}`)
+  // if no orgs for this user throw a not found
+  // but much better if we redirect them to create
+  // a new organization.
+  if (!tenants[0]) {
+    return redirect("/tenants/new")
   }
 
-  console.log("no database call")
-
-  // if cookie is available then do not perform a dabase trip
-  // just return the available cookie.
-  return redirect(`/${currentOrganization}`)
+  // then redirect to the first organization available.
+  return redirect(`/${tenants[0].id}`)
 }
